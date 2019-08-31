a3eca2ae8edeaf7ecb8ed04144ee7d3fe2efbdf8
Autoscore <43796410+autoscore@users.noreply.github.com>
2019-08-31 00:17:33 -0600

    Update pwc.R

--- R/pwc.R
+++ R/pwc.R
@@ -3,28 +3,26 @@
 #' Obtain the percent words correct from a data.frame produced by `autoscore()`.
 #'
 #' @param data data.frame containing the target, response, and autoscore columns as produced by the function `autoscore()`.
+#' @param id the id variable(s) to summarize the PWC for
 #'
 #' @return data.frame of the percent correct for each line in `data`
 #'
-#'
-#' @importFrom dplyr pull
-#' @importFrom dplyr rename_all
+#' @import rlang
+#' @importFrom purrr map
+#' @importFrom purrr map_dbl
+#' @import dplyr
 #' @import stringr
-#' @importFrom dplyr mutate
 #'
 #' @export
-pwc <- function(data){
-  target_words <- data %>%
-    dplyr::rename_all(tolower) %>%
-    dplyr::pull(target) %>%
-    stringr::str_split(pattern = " ") %>%
-    purrr::map(~stringr::str_remove_all(.x, pattern = "[[:punct:]]")) %>%
-    purrr::map_dbl(~length(.x))
-  correct_words <- data %>%
-    dplyr::pull(autoscore)
+pwc <- function(data, id){
+  id <- rlang::enquo(id)
 
   data %>%
-    dplyr::mutate(pwc = (correct_words / target_words) * 100)
+    dplyr::rename_all(tolower) %>%
+    dplyr::mutate(target = stringr::str_split(target, pattern = " ") %>%
+                    purrr::map(~stringr::str_remove_all(.x, pattern = "[[:punct:]]")) %>%
+                    purrr::map_dbl(~length(.x))) %>%
+    dplyr::group_by(!! id) %>%
+    dplyr::summarise(pwc = sum(autoscore) / sum(target) * 100)
 }
-
 
